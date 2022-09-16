a82ea9d6f7c461e3b3f65cc857e80bb6db0a899f
Shuhei Hagiwara <s.hagiwara@student.fontys.nl>
2022-09-16 14:18:45 +0200

    initial commit

---
+++ src/main/java/nl/fontys/s3/bvbforum/BvbForumApplication.java
@@ -0,0 +1,13 @@
+package nl.fontys.s3.bvbforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BvbForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BvbForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/fontys/s3/bvbforum/BvbForumApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.fontys.s3.bvbforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BvbForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
