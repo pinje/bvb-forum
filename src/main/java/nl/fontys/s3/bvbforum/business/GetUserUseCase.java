890d27a351495f751746bc944d0c07575aece3ff
Shuhei Hagiwara <s.hagiwara@student.fontys.nl>
2022-11-03 05:31:45 +0100

    Remove unused imports

--- src/main/java/nl/fontys/s3/bvbforum/business/GetUserUseCase.java
+++ src/main/java/nl/fontys/s3/bvbforum/business/GetUserUseCase.java
@@ -1,9 +1,6 @@
 package nl.fontys.s3.bvbforum.business;
 
-import nl.fontys.s3.bvbforum.domain.User;
 import nl.fontys.s3.bvbforum.persistence.entity.UserEntity;
-
-import java.util.Optional;
 
 public interface GetUserUseCase {
     UserEntity getUserById(long userId);
