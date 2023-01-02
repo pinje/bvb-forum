789163430b507710778ec282dde1a0a258165b83
Shuhei Hagiwara <s.hagiwara@student.fontys.nl>
2023-01-02 21:28:41 +0100

    Add player entity and repo

---
+++ src/main/java/nl/fontys/s3/bvbforum/persistence/PlayerRepository.java
@@ -0,0 +1,7 @@
+package nl.fontys.s3.bvbforum.persistence;
+
+import nl.fontys.s3.bvbforum.persistence.entity.PlayerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
+}
---
+++ src/main/java/nl/fontys/s3/bvbforum/persistence/entity/PlayerEntity.java
@@ -0,0 +1,39 @@
+package nl.fontys.s3.bvbforum.persistence.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "player")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlayerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotBlank
+    @Length(min = 2, max = 50)
+    @Column(name = "firstname")
+    private String firstname;
+
+    @NotBlank
+    @Length(min = 2, max = 50)
+    @Column(name = "lastname")
+    private String lastname;
+
+    @NotBlank
+    @Length(max = 2)
+    @Column(name = "position")
+    private String position;
+}
