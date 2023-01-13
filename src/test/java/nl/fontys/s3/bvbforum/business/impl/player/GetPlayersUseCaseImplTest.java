79279d693360fcdf24aa54aef966e506281cb89f
Shuhei Hagiwara <s.hagiwara@student.fontys.nl>
2023-01-13 23:20:10 +0100

    Add missing test for new method

--- src/test/java/nl/fontys/s3/bvbforum/business/impl/player/GetPlayersUseCaseImplTest.java
+++ src/test/java/nl/fontys/s3/bvbforum/business/impl/player/GetPlayersUseCaseImplTest.java
@@ -1,11 +1,12 @@
 package nl.fontys.s3.bvbforum.business.impl.player;
 
 import nl.fontys.s3.bvbforum.business.exception.player.PlayerDoesntExistException;
+import nl.fontys.s3.bvbforum.business.exception.rating_post.RatingPostDoesntExistException;
 import nl.fontys.s3.bvbforum.domain.Player;
 import nl.fontys.s3.bvbforum.domain.response.player.GetPlayersResponse;
 import nl.fontys.s3.bvbforum.persistence.PlayerRepository;
-import nl.fontys.s3.bvbforum.persistence.entity.PlayerEntity;
-import nl.fontys.s3.bvbforum.persistence.entity.PositionEnum;
+import nl.fontys.s3.bvbforum.persistence.RatingPostPlayerRepository;
+import nl.fontys.s3.bvbforum.persistence.entity.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -23,6 +24,8 @@ import static org.mockito.Mockito.*;
 class GetPlayersUseCaseImplTest {
     @Mock
     private PlayerRepository playerRepository;
+    @Mock
+    private RatingPostPlayerRepository ratingPostPlayerRepository;
     @InjectMocks GetPlayersUseCaseImpl getPlayersUseCase;
 
     @Test
@@ -62,6 +65,62 @@ class GetPlayersUseCaseImplTest {
     }
 
     @Test
+    void GetAll_ExistingPlayers_ReturnsAllPlayers_ByRatingPostId() {
+        // given
+        PlayerEntity playerEntityOne = PlayerEntity.builder()
+                .id(1L)
+                .firstname("marco")
+                .lastname("reus")
+                .position(PositionEnum.MF)
+                .build();
+
+        PlayerEntity playerEntityTwo = PlayerEntity.builder()
+                .id(2L)
+                .firstname("jude")
+                .lastname("bellingham")
+                .position(PositionEnum.MF)
+                .build();
+
+        RatingPostEntity ratingPostEntity = RatingPostEntity.builder()
+                .id(111L)
+                .start_year(2022)
+                .end_year(2023)
+                .matchday(1)
+                .opponent("Schalke04")
+                .tournament(TournamentEnum.BUNDESLIGA)
+                .build();
+
+        RatingPostPlayerEntity ratingPostPlayerEntityOne = RatingPostPlayerEntity.builder()
+                        .id(1L)
+                        .player(playerEntityOne)
+                        .ratingPost(ratingPostEntity)
+                        .build();
+
+        RatingPostPlayerEntity ratingPostPlayerEntityTwo = RatingPostPlayerEntity.builder()
+                .id(2L)
+                .player(playerEntityTwo)
+                .ratingPost(ratingPostEntity)
+                .build();
+
+        // set up mock objects
+        when(ratingPostPlayerRepository.findRatingPostPlayerEntitiesByRatingPostId(111L))
+                .thenReturn(List.of(ratingPostPlayerEntityOne, ratingPostPlayerEntityTwo));
+
+        // call the method
+        GetPlayersResponse actualResult = getPlayersUseCase.getPlayersByRatingPostId(111L);
+
+        // then
+        Player one = Player.builder().id(1L).firstname("marco").lastname("reus").position(PositionEnum.MF).build();
+        Player two = Player.builder().id(2L).firstname("jude").lastname("bellingham").position(PositionEnum.MF).build();
+        GetPlayersResponse expectedResult = GetPlayersResponse.builder().players(List.of(one, two)).build();
+
+        assertEquals(expectedResult, actualResult);
+
+        // verify
+        verify(ratingPostPlayerRepository).findRatingPostPlayerEntitiesByRatingPostId(111L);
+    }
+
+    @Test
     void GetAll_ExistingPlayers_NoPlayersExist_ThrowsException() throws PlayerDoesntExistException {
         // given
 
@@ -79,5 +138,25 @@ class GetPlayersUseCaseImplTest {
 
         // verify
         verify(playerRepository, times(1)).findAll();
+    }
+
+    @Test
+    void GetAll_ExistingPlayers_ByRatingPostId_NoRatingPostExist_ThrowsException() throws RatingPostDoesntExistException {
+        // given
+
+        // set up mock objects
+        when(ratingPostPlayerRepository.findRatingPostPlayerEntitiesByRatingPostId(111L)).thenReturn(List.of());
+
+        // when
+        ResponseStatusException exception = assertThrows(RatingPostDoesntExistException.class, () -> {
+            getPlayersUseCase.getPlayersByRatingPostId(111L);
+        });
+
+        // then
+        assertEquals("RATING_POST_DOESNT_EXIST", exception.getReason());
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getStatus());
+
+        // verify
+        verify(ratingPostPlayerRepository, times(1)).findRatingPostPlayerEntitiesByRatingPostId(111L);
     }
 }
